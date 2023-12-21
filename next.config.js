9aeb827638fab84fba6522fca599263866b4e8bf
shinjongung <whddnd0728@gmail.com>
2023-12-21 15:15:58 +0900

    조회수 기능 추가

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 const { withContentlayer } = require('next-contentlayer')
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    scrollRestoration: true,
-  },
-}
+const nextConfig = {}
 
 module.exports = withContentlayer(nextConfig)
